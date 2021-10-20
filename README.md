c2806f3e899f1bb327d6e8b13bcab988ce9bf709
Michala Nybroe <michalanim@gmail.com>
2021-10-20 19:25:57 +0200

    add readme file and start.sql file

---
+++ README.md
@@ -0,0 +1,10 @@
+#Little Birdie
+
+##Set up
+To use program run `start.sql` file creating a table and test user.  
+In `application.properties` you will need to adjust settings for your specific
+use.
+
+##Contributors
+[@kamilleNikolajsen](https://github.com/kamilleNikolajsen)  
+[@MichalaNybroe](https://github.com/MichalaNybroe)  
---
+++ start.sql
@@ -0,0 +1,8 @@
+CREATE TABLE `users` (
+                         `user_id` int NOT NULL AUTO_INCREMENT,
+                         `user_name` varchar(45) NOT NULL,
+                         `user_password` varchar(45) NOT NULL,
+                         PRIMARY KEY (`user_id`)
+);
+
+INSERT INTO users(user_name, user_password) VALUES ('test', 'test');
