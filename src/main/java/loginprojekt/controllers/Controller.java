86d16436a713f911cd9998a807fb7d864eb0b1c4
Michala Nybroe <michalanim@gmail.com>
2021-10-20 15:14:56 +0200

    add postmaps in controller and query for checkuser

--- src/main/java/loginprojekt/controllers/Controller.java
+++ src/main/java/loginprojekt/controllers/Controller.java
@@ -1,9 +1,14 @@
 package loginprojekt.controllers;
 
+import loginprojekt.services.UserService;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @org.springframework.stereotype.Controller
 public class Controller {
+    UserService userService;
 
     @GetMapping("/")
     public String home() {
@@ -23,5 +28,20 @@ public class Controller {
     @GetMapping("/createUser")
     public String create() {
         return "createUser";
+    }
+
+    @PostMapping("/")
+    public String createdUser(@RequestParam String name, @RequestParam String password, Model model) {
+        userService.createUser(name, password);
+        model.addAttribute("user", userService.getUser());
+        return "index";
+    }
+
+    @PostMapping("/profilePage")
+    public String logIn(@RequestParam String name, @RequestParam String password, Model model) {
+        userService.checkUser(
+                "SELECT user.user_name FROM users WHERE user_name = " + name + " && user_password = " + password
+        );
+        model.addAttribute("user", )
     }
 }
--- src/main/java/loginprojekt/services/UserService.java
+++ src/main/java/loginprojekt/services/UserService.java
@@ -1,7 +1,6 @@
 package loginprojekt.services;
 
 import loginprojekt.models.User;
-
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -9,33 +8,46 @@ import java.sql.SQLException;
 
 public class UserService {
 
-  private User user;
+    private User user;
 
-  public void createUser(String userName, String password){
-    this.user = new User(userName, password);
-  }
+    public void createUser(String userName, String password) {
+        String insert = "INSERT INTO users(user_name, user_password) VALUES(?,?)";
+        Connection connection = DBManager.getConnection();
+        PreparedStatement preparedStatement;
+        try {
+            preparedStatement = connection.prepareStatement(insert);
+            preparedStatement.setString(1, userName);
+            preparedStatement.setString(2, password);
+            preparedStatement.executeUpdate();
 
-  public User getUser(){
-    return this.user;
-  }
-
-  public boolean checkUser(String query){
-    Connection c = DBManager.getConnection();
-    PreparedStatement preparedStatement;
-
-    try {
-      preparedStatement = c.prepareStatement(query);
-      ResultSet resultSet = preparedStatement.executeQuery();
-
-      //HVAD SKAL VI BRUGE RESULTSET TIL????
-
-      return true;
-
-    }catch (SQLException e){
-      System.out.println(e.getErrorCode());
+            this.user = new User(userName, password);
+        } catch (SQLException err) {
+            err.printStackTrace();
+        }
     }
 
-    return false;
-  }
+    public User getUser() {
+        return this.user;
+    }
+
+    public boolean checkUser(String query) {
+        //"SELECT user.user_name FROM users WHERE user_name = " + name + " && user_password = " + password + "";
+        Connection c = DBManager.getConnection();
+        PreparedStatement preparedStatement;
+
+        try {
+            preparedStatement = c.prepareStatement(query);
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            //HVAD SKAL VI BRUGE RESULTSET TIL????
+
+            return true;
+
+        } catch (SQLException e) {
+            System.out.println(e.getErrorCode());
+        }
+
+        return false;
+    }
 }
 
