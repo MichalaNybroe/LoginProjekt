22c6c1edd414b369323ae927e950e7f71ef6adf0
Michala Nybroe <michalanim@gmail.com>
2021-10-24 21:54:51 +0200

    fix calc getmapping and post

--- src/main/java/loginprojekt/controllers/CalculatorController.java
+++ src/main/java/loginprojekt/controllers/CalculatorController.java
@@ -1,13 +1,18 @@
 package loginprojekt.controllers;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.view.RedirectView;
 
+import javax.servlet.http.HttpSession;
+
+@Controller
 public class CalculatorController {
-    @GetMapping("/calculator")
+    @PostMapping("/calculating")
     public ModelAndView calculator(@RequestParam int first, @RequestParam int second, @RequestParam String operator, Model model) {
         int result;
         if (operator.equals("+")) {
@@ -25,8 +30,12 @@ public class CalculatorController {
     }
 
 
-    @PostMapping("/calculating")
-    public ModelAndView calculating() {
-        return new ModelAndView("calculator");
+    @GetMapping("/calculator")
+    public ModelAndView calculating(HttpSession session) {
+        if (session.getAttribute("username") != null) {
+            return new ModelAndView("calculator");
+        } else {
+            return new ModelAndView(new RedirectView("/"));
+        }
     }
 }
